8cb3cceae7b21f7432dd5291bb182411e81574cc
Landon Cayia <landon.cayia@gmail.com>
2022-07-30 12:04:42 -0400

    Add Codable support to User class manually
    This was implemented via three additions: first, an enumerated type
    that conforms to CodingKey protocol, where each case is data we want
    to save/load; second, the required custom initializer that takes an
    instance of Decoder; and finally, an encoding function. This enables
    saving and loading data in a custom manner.

--- P10-CupcakeCorner/CupcakeCorner/ContentView.swift
+++ P10-CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,6 +7,30 @@
 
 import SwiftUI
 
+class User: ObservableObject, Codable {
+    // enum conforming to CodingKey tells Swift what to saev and load via cases
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Landon Cayia"
+    
+    // required enforces that subclasses must override the initializer
+    // alternatively, mark class as final
+    required init(from decoder: Decoder) throws {
+        // ask the Decoder instance for a container matching keys; crash if not found
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        // read values from container
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    // opposite of decoder
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
