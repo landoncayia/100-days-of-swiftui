1b5fae18032504a064201ce9dc7e0289d44cf25e
Landon Cayia <landon.cayia@gmail.com>
2022-07-30 11:04:33 -0400

    Create Project 10: CupcakeCorner

---
+++ 14-CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Landon Cayia on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
